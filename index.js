6aee291f16b42f00e5295b1f928c8009f2bb762c
Halim Choi <hchoicareer@gmail.com>
2023-03-12 21:37:18 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+const mainElement = document.getElementById("main");
+const newHeader = document.createElement("h1");
+mainElement.remove();
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = "Halim is the champion";
